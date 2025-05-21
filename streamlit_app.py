2941a6c653627b4d814c92cc1f585a2f186e22a3
bienyuu <abianyu04@gmail.com>
2025-05-21 14:56:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,15 @@
 import streamlit as st
 
-st.title("🎈warpolno1pastigacor")
+st.title("🎈warpol-no-1-pasti-gacor")
 st.write(
-    "anjaygantengbangetgw"
+    "anjay-ganteng-banget-gw"
 )
 st.image("IMG_20250415_074235.jpg") 
 
 st. title("aplikasi sederhana")
 st. header("aplikasi mengecek nilai genap/ganjil")
-angka st.number_input("Tulis sebuah Angka:", value-0, step-1)
+angka = 
+st.number_input("Tulis sebuah Angka:", value-0, step-1)
 
 if (angka % 2) == 0:
 st.write(f"{angka} adalah Bilangan Genap")
